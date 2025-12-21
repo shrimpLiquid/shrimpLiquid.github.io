b5230110c04a2ba630d5e83487c800116721e170
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2025-12-20 21:21:19 -0800

    Update trans.js

--- trans.js
+++ trans.js
@@ -1,9 +1,12 @@
 function convertToHtml() {
     const inputText = document.getElementById('inputText').value;
     // Simple conversion: split by newlines and wrap each paragraph in <p> tags
-    const htmlOutput = inputText
-    
+    htmlOutput = "."+inputText.replace(/ /i, ".");
+    htmlOutput = htmlOutput.replace(/([aeiqouyh])([aeiqouyh])/g, "$11$2");
+    htmlOutput = htmlOutput.replace(/(\.[aeiqouyh])/g, "$11");
+    htmlOutput = htmlOutput.replace(/([rtsfjkzxcv][aeiqouyh])/g, "$12");
+    htmlOutput = htmlOutput.replace(/21/g, "1");
+    htmloutput = htmlOutput = htmlOutput.replace(/^./, "");
+  
     document.getElementById('outputCode').value = htmlOutput;
 }
-
-
