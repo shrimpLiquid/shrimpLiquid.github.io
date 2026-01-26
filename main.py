08562adc256e9b2f0cfd506dfcb5dfb526f69ec6
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-01-25 16:25:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def get_image():
     # 1. Open the local image file on the server
     draw = ImageDraw.Draw(image) 
 
-font = ImageFont.truetype("mononoki-Regular.ttf", 45) 
-
-for X in range(5):
-    for Y in range(5):    
-        if not X == Y == 2:
-            text = choice(lines)
-            lines.remove(text)
-            newtext=text.replace(">","\n")
-            l = text.count(">")
-            draw.text(((409*X)+5, (409*Y)+5+(200-(l*20))), newtext,fill=(randint(0,200),randint(0,200),randint(0,200)), font = font, align ="left") 
+    font = ImageFont.truetype("mononoki-Regular.ttf", 45) 
+    
+    for X in range(5):
+        for Y in range(5):    
+            if not X == Y == 2:
+                text = choice(lines)
+                lines.remove(text)
+                newtext=text.replace(">","\n")
+                l = text.count(">")
+                draw.text(((409*X)+5, (409*Y)+5+(200-(l*20))), newtext,fill=(randint(0,200),randint(0,200),randint(0,200)), font = font, align ="left") 
 
     buf = io.BytesIO()
     img.save(buf, format='PNG')
