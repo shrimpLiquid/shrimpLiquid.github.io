f4f3b9f31841c2b8d7c57312498fb39c27196b89
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-01-25 17:08:51 -0800

    Update main.py
    Refactor lines variable to be local within get_image function.

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ import base64
 app = Flask(__name__)
 CORS(app)
 file = open("LINES.txt",encoding="utf8")
-global lines
-lines = file.readlines()  
-temp = lines
 img = Image.open("bingo.png") 
 @app.route('/get-processed-image')
 def get_image():
+    lines = file.readlines()  
+    temp = lines
     # 1. Open the local image file on the server
     draw = ImageDraw.Draw(img) 
 
@@ -27,7 +26,6 @@ def get_image():
                 newtext=text.replace(">","\n")
                 l = text.count(">")
                 draw.text(((409*X)+5, (409*Y)+5+(200-(l*20))), newtext,fill=(randint(0,200),randint(0,200),randint(0,200)), font = font, align ="left")
-    lines = temp
 
     buf = io.BytesIO()
     img.save(buf, format='PNG')
