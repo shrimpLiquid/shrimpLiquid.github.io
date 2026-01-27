9e986def9e0c468ef8ecc67d8bc0d61bf555a53f
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-01-26 21:03:24 -0800

    Change loop range from 0 to 1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_image():
         #font = ImageFont.truetype(os.path.abspath(__file__).replace("main.py","mononoki-Regular.ttf"), 45) 
     except:
         font = ImageFont.load_default()
-    for XX in range(0):
+    for XX in range(1):
         for YY in range(2):    
             for X in range(5):
                 for Y in range(5):    
