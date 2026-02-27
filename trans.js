875c8d9bdb13ccf0d8b09368101ca6a435c6280f
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-02-27 13:42:18 -0800

    Update trans.js

--- trans.js
+++ trans.js
@@ -2,6 +2,7 @@ function convertToHtml() {
     const inputText = document.getElementById('inputText').value;
     // Simple conversion: split by newlines and wrap each paragraph in <p> tags
     htmlOutput = "."+inputText.replace(/ /g, ".");
+    htmlOutput = htmlOutput.toLowerCase();
     htmlOutput = htmlOutput.replace(/([aeiqouyh])([aeiqouyh])/g, "$11$2");
     htmlOutput = htmlOutput.replace(/(\.[aeiqouyh])/g, "$11");
     htmlOutput = htmlOutput.replace(/([rtsfjkeqyzxcv][aeiqouyh])/g, "$12");
