085cb83dc43d2b301a17f674a66e4257f63450cf
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-04-23 18:51:56 -0700

    onaoind

--- ball/main.py
+++ ball/main.py
@@ -1,55 +1,67 @@
 # /// script
 # dependencies = [
-#     "cffi",
-#     "raylib"
+#   "raylib",
 # ]
 # ///
-
 import asyncio
 from pyray import *
-from math import cos, sin, radians as rad
-
-def scale(value, istart, istop, ostart, ostop):
-    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
 
 x,y,v,r,color = 0,1,2,3,4
-
-
-guys = [[500,800,[0,0],0,0]]
-
+guys = [[500,100,[0.0,0.0],0,BLUE]]
+platforms = [[100,600,1240,700],[800,500,900,550]]
+speed = 4
+gravity = 1.5
 
 async def main():
     init_window(1280, 720, "soup")
+    set_target_fps(60)
+    
     while not window_should_close():
-        set_target_fps(60)
         begin_drawing()
         clear_background(WHITE)
-        
+
         for g in guys:
-            draw_rectangle(int(g[x]-30),int(g[y]-30),60,60,RED)
-
-            if g[y] < 600:
-                g[v][y] += 5
+            if g[v][y] < 0:
+                g[v][y] += gravity
             else:
-                g[v][y] = 0
-
-            if is_key_down(KeyboardKey.KEY_A):
-                g[v][x] += -5
-            if is_key_down(KeyboardKey.KEY_D):
-                g[v][x] += +5
-            if is_key_pressed(KeyboardKey.KEY_W) and g[v][y] < 0.01:
-                g[v][y] += -50
-
+                g[v][y] += gravity*2
+            if is_key_down(KeyboardKey.KEY_A): g[v][x] -= speed
+            if is_key_down(KeyboardKey.KEY_D): g[v][x] += speed
+            
             g[x] += g[v][x]
-            g[v][x] = g[v][x]/1.5
+            for p in platforms:
+                rect = Rectangle(p[x], p[y], p[2]-p[x], p[3]-p[y])
+                if check_collision_circle_rec(Vector2(g[x], g[y]), 30, rect):
+                    if g[v][x] > 0: g[x] = p[x] - 30.1
+                    elif g[v][x] < 0: g[x] = p[2] + 30.1
+                    g[v][x] = 0
+
             g[y] += g[v][y]
+            on_ground = False
+            for p in platforms:
+                rect = Rectangle(p[x], p[y], p[2]-p[x], p[3]-p[y])
+                if check_collision_circle_rec(Vector2(g[x], g[y]), 30, rect):
+                    if g[v][y] > 0:
+                        g[y] = p[y] - 30.1
+                        g[v][y] = 0
+                        on_ground = True
+                    elif g[v][y] < 0:
+                        g[y] = p[3] + 30.1
+                        g[v][y] = 0
 
+            g[v][x] *= 0.75
 
+            if is_key_pressed(KeyboardKey.KEY_W) and on_ground:
+                g[v][y] = -25
+
+            draw_circle(int(g[x]), int(g[y]), 30, g[color])
+
+        for p in platforms:
+            draw_rectangle(p[x], p[y], p[2]-p[x], p[3]-p[y], BLACK)
 
         end_drawing()
-
         await asyncio.sleep(0)
-    close_window()
 
+    close_window()
 
 asyncio.run(main())
