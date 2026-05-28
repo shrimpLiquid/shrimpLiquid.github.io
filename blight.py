8d89e631a5db57510f97997b42765ca4c286db48
shrimpLiquid <144174587+shrimpLiquid@users.noreply.github.com>
2026-05-28 15:40:15 -0700

    fix

--- blight.py
+++ blight.py
@@ -1,13 +1,13 @@
 import pyxel  
 #from random import shuffle
-sm = 0.6 # size multplier
+sm = 1.2 # size multplier
 def clamp(n, minn, maxn):
 	return max(min(maxn, n), minn)
 class App:
 	def __init__(self):
 		pyxel.init(int(100*sm), int(100*sm),fps=30)
-		self.p = [[90,50,8],[10,50,5],[50,10,11],[50,90,10]]#4
-		#self.p = [[10,10,1],[50,10,2],[90,10,3],[10,50,8],[50,50,9],[90,50,4],[10,90,7],[50,90,6],[90,90,5]]
+		#self.p = [[90,50,8],[10,50,5],[50,10,11],[50,90,10]]#4
+		self.p = [[10,10,1],[50,10,2],[90,10,3],[10,50,8],[50,50,9],[90,50,4],[10,90,7],[50,90,6],[90,90,5]]
 		pyxel.colors.from_list([0x000000,0xff4444,0xff7f44,0xffff44,0x44ff44,0x44ffff,0x4444ff,0x7f44ff,0xff44ff,0xffffff])
 		pyxel.run(self.update, self.draw)
 		
@@ -51,7 +51,6 @@ class App:
 
 			#p[0],p[1]=p[0]%(100*sm),p[1]%(100*sm)
 			p[0],p[1]=clamp(p[0],0,(100*sm)),clamp(p[1],0,(100*sm))
-		print(len(self.p))
 		
 
 App()
